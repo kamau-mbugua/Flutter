5c0a0c1b4247bac743dc6a28b63fbe98dc4abe45
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-04-18 00:14:10 +0300

    added dependency english_words

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -2,7 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +21,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Text('Hello World'),
+          //child: Text('Am Kamau')
         ),
       ),
     );
