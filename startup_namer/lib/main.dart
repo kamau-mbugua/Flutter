ade0869c4e50d8c4f508973a758c849df3fccaaa
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-04-18 01:07:11 +0300

    updates of the _RandomWordState

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:html';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -37,13 +38,53 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-  final _suggestions =<WordPair>[];
+  final _suggestions = <WordPair>[];
   final _biggerFont = TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    
-    return Text(wordPair.asPascalCase);
+    // final wordPair = WordPair.random();
+    //
+    // return Text(wordPair.asPascalCase);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: /*The itemBuilder callback is called once per
+        suggested word pairing, and places each suggestion
+        into a ListTile row. For even rows, the function adds
+        a ListTile row for the word pairing. For odd rows,
+        the function adds a Divider widget to visually separate the entries.
+        Note that the divider might be difficult to see on smaller devices.*/
+            (context, i) {
+          if (i.isOdd) return Divider(); /*Add a one-pixel-high divider
+          widget before each row in the ListView.*/
+
+          final index = i ~/ 2; /*The expression i ~/ 2 divides i by 2
+          and returns an integer result. For example: 1, 2, 3, 4, 5
+          becomes 0, 1, 1, 2, 2. This calculates the
+          actual number of word pairings in the
+          ListView, minus the divider widgets.*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*If you’ve reached
+            the end of the available word pairings,
+            then generate 10 more and add them to the suggestions list.*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(pair.asPascalCase,style: _biggerFont,),
+    );
   }
 }
 
