a94be4c97adecb24e065da5ab8807bb0f26e4c52
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-04-19 00:30:08 +0300

    primaryColor to orange

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       title: 'Startup Name Generator',
 
       theme: ThemeData(
-        primaryColor: Colors.blue ,
+        primaryColor: Colors.orange ,
       ),
       home: RandomWords(),
 
