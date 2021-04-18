6d4836410dd9a06dea9465e50c60f06b924e6d5f
kamau-mbugua <kamau_mbugua@yahoo.com>
2021-04-19 00:00:06 +0300

    App Interactivity

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -81,10 +81,14 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved  = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.purple: null,
       ),
     );
   }
